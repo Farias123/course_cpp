e0100117223c247f1f66f4d80ecb924452b3ad1c
Farias123 <vitorcostafarias@gmail.com>
2025-06-02 11:02:51 -0300

    control simulation from console

--- animate.py
+++ animate.py
@@ -10,11 +10,10 @@ def parse_header(header):
         data_dict[str.strip(k)] = float(v)
     return data_dict
 
-def main():
+def main(y_data_file):
     scene.background = color.white
 
-
-    with open("./y_positions_calculated/analytical_solution.txt") as file:
+    with open(y_data_file) as file:
         content = file.read().split("\n")
 
     header = content[0]
@@ -22,8 +21,7 @@ def main():
     y_list = [float(x) for x in content[1:]]
 
 
-
-    water = box(pos=vector(0, params.get("h_water") - params.get("cube_height")*5/2, 0), length=50, height=params.get("cube_height")*5, width=50, color=color.blue, opacity=0.4)
+    water = box(pos=vector(0, - params.get("cube_height")*5/2, 0), length=50, height=params.get("cube_height")*5, width=50, color=color.blue, opacity=0.4)
     # water height -water_cube_height/2 in order for surface to be on h_water height
 
     solid = box(pos=vector(params.get("x0"), y_list[0], params.get("z0")), length=params.get("cube_length"),
@@ -33,13 +31,12 @@ def main():
     scene.forward = vector(-8.61595, -2.73272, 6.87733)
     scene.range = 22.96683
     # TODO melhorar câmera
-    # TODO plotar ambos os cálculos e mostrar o tempo que cada um demorou para ser calculado
 
     dt = params.get("dt")
     for y in y_list:
-        sleep(dt)
         solid.pos.y = y
+        sleep(dt)
 
 if __name__ == "__main__":
-    main()
+    main("./y_positions_calculated/numerical_solution.txt")
 
---
+++ main.py
@@ -0,0 +1,64 @@
+import subprocess
+import animate
+
+
+def full_process():
+    # TODO capture output
+    subprocess.run(["g++", "./main.cpp", "-o",  "main"], capture_output=True, text=True)
+
+    while True:
+        execute_cpp_calculations()
+
+        file = choose_data_file()
+        animate.main(file)
+
+        print("Do you want to do another simulation (y or n):")
+        user_input = input(">")
+
+        if str.lower(user_input) == 'y':
+            continue
+        else:
+            break
+
+
+
+def choose_data_file():
+    print("""The analytical method has a perfect solution and should be used for simple cases (when the solid is never
+    completely under or out of the water), whereas the numerical method is an approximation of the solution that works
+    for more complex cases but diverges if the interval dt is not small enough.""")
+    while True:
+        print("You want to watch the numerical(1) or analytical(2) solution?")
+        user_input = int(input('>'))
+
+        if user_input == 1:
+            return "./y_positions_calculated/numerical_solution.txt"
+
+        elif user_input == 2:
+            return "./y_positions_calculated/analytical_solution.txt"
+
+        print("Not a valid choice")
+
+
+def execute_cpp_calculations():
+    print("Would you like to choose custom values for the simulation (y or n):")
+    user_input = input('>')
+
+    if str.lower(user_input) == 'y':
+        param_names = ["initial_x", "initial_y", "initial_z", "initial_y_speed", "simulation_time", "solid_width",
+                       "solid_length", "solid_height", "cube_mass", "dt"]
+        params = ["./main", "1"]
+
+        for param_name in param_names:
+            print(f"Enter {param_name}:")
+            param_entered = input()
+            params.append(param_entered)
+
+        output = subprocess.run(params, capture_output=True, text=True)
+    else:
+        output = subprocess.run(["./main"], capture_output=True, text=True)
+
+    return output
+
+
+if __name__ == "__main__":
+    full_process()
